6511b9b31469d96b8da73ab7f638c4dfb6fc5993
kharinna22 <68131024+kharinna22@users.noreply.github.com>
2020-11-05 02:14:00 -0300

    Barra de navegation-home

--- cliente/src/app/components/navigation-home/navigation-home.component.ts
+++ cliente/src/app/components/navigation-home/navigation-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {HomeServiceService} from '../../services/homeService/home-service.service'
 
 @Component({
   selector: 'app-navigation-home',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-home.component.css']
 })
 export class NavigationHomeComponent implements OnInit {
+  errorMsg='';
+  equipos=null;
 
-  constructor() { }
+  constructor(private _homeService: HomeServiceService,private router: Router) { 
+    this.obtenerEquipoUsuario();
+  }
 
   ngOnInit(): void {
+  }
+
+  obtenerEquipoUsuario(){
+    this._homeService.obtenerEquiposUsuario()
+    .subscribe(
+      data => {(this.equipos = data),console.log(this.equipos)},
+      error => {
+        this.errorMsg=error.statusText;
+        console.log("Error al recibir los equipos");
+      }
+    )
+  }
+
+  cerrarSesion(){
+    this._homeService.logout().subscribe(
+      data=>{
+        console.log("Dato recibido de servidor: "+data.message);
+        this.router.navigateByUrl('/welcome');
+      },
+      error=>{
+        this.errorMsg = error.statusText
+        console.log("Error, no se recibe data")
+      }
+    );
   }
 
 }
