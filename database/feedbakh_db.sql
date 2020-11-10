bbbe3a8eaa1c2cacee8013d1688ec50adbb77bc7
noidfrx <58480414+noidfrx@users.noreply.github.com>
2020-11-10 19:15:22 -0300

    Database con índice tutorial

--- database/feedbakh_db.sql
+++ database/feedbakh_db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2020 a las 23:45:30
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.11
+-- Tiempo de generación: 10-11-2020 a las 23:13:54
+-- Versión del servidor: 10.1.40-MariaDB
+-- Versión de PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +32,7 @@ CREATE TABLE `amigo` (
   `idAmigo` int(255) NOT NULL,
   `idColaborador1` int(255) NOT NULL,
   `idColaborador2` int(255) NOT NULL,
-  `aceptado` int(255) NOT NULL DEFAULT 0
+  `aceptado` int(255) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -125,68 +126,70 @@ CREATE TABLE `colaborador` (
   `fotoPerfil` varchar(255) DEFAULT '0',
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `fechaCreacion` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+  `fechaCreacion` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `tutorial` int(255) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `colaborador`
 --
 
-INSERT INTO `colaborador` (`idColaborador`, `nombre`, `apellidos`, `fotoPerfil`, `email`, `password`, `fechaCreacion`) VALUES
-(1, 'admin', 'admin', '0', 'admin@admin.com', 'admin', '2020-10-31 17:40:06.000000'),
-(2, 'Eduardo', 'Ibacache González', '0', 'eduardo@gmail.com', '1235', '2020-10-31 17:40:50.430169'),
-(3, 'Karina', 'Hermosilla Moraga', '0', 'kari@kari.com', 'kari', '2020-11-03 02:15:33.655545'),
-(4, 'Felipe', 'Javier', '0', 'Ad@ad.com', 'jiji', '2020-11-04 20:42:24.519724'),
-(5, 'Omar', 'Valdés', '0', 'venenatis@elementum.ca', 'Nki98MN', '2021-02-11 01:37:01.000000'),
-(6, 'Adela', 'Sepúlveda', '0', 'tempor.diam@felispurusac.net', 'Zce68OY', '2020-01-31 12:48:08.000000'),
-(7, 'Pilar', 'Campos', '0', 'tempus.non.lacinia@ullamcorpermagnaSed.net', 'Mkg27FP', '2019-12-21 18:54:18.000000'),
-(8, 'Eugenio', 'Muñoz', '0', 'risus.odio.auctor@tortorNunccommodo.com', 'Xrk40OK', '2021-10-10 04:54:03.000000'),
-(9, 'Miley', 'Figueroa', '0', 'vitae.sodales.at@placerateget.co.uk', 'Rxd56IA', '2021-02-23 10:42:18.000000'),
-(10, 'Yeremy', 'Silva', '0', 'Nulla.eget.metus@arcuac.net', 'Jrz65DT', '2019-11-29 21:02:13.000000'),
-(11, 'Isaias', 'Araya', '0', 'cubilia.Curae@etpede.com', 'Etx46JQ', '2021-01-09 12:44:55.000000'),
-(12, 'Patrick', 'Sepúlveda', '0', 'dui@rutrumeuultrices.ca', 'Nba93HV', '2019-12-15 01:38:44.000000'),
-(13, 'Ademir', 'Jiménez', '0', 'cursus.diam@mattis.edu', 'Mod02XD', '2021-04-21 07:24:06.000000'),
-(14, 'Joao', 'Martínez', '0', 'aliquet.libero@eutellusPhasellus.org', 'Rub72FA', '2020-07-30 15:43:58.000000'),
-(15, 'Cristián', 'Ortega', '0', 'ut.nisi@senectus.co.uk', 'Qec12PZ', '2021-02-04 18:15:29.000000'),
-(16, 'Yanira', 'Vega', '0', 'nec@magnis.co.uk', 'Nty25JM', '2020-11-10 20:13:12.000000'),
-(17, 'Lorenzo', 'Vásquez', '0', 'Nullam@Nuncquisarcu.net', 'Bit58TU', '2020-09-07 23:15:00.000000'),
-(18, 'Maria', 'Muñoz', '0', 'augue.ut.lacus@sapien.edu', 'Wnz15IA', '2021-05-26 03:49:12.000000'),
-(19, 'Thiago', 'Alvarado', '0', 'nulla@ultriciesligulaNullam.ca', 'Tsd73VK', '2020-08-02 06:44:49.000000'),
-(20, 'Jean', 'Silva', '0', 'Donec.felis@sollicitudincommodo.co.uk', 'Lqy79KE', '2020-01-02 18:45:26.000000'),
-(21, 'Michelle', 'Carvajal', '0', 'Suspendisse@Morbi.org', 'Wqs37IT', '2020-11-11 21:17:06.000000'),
-(22, 'Amaru', 'Salazar', '0', 'lobortis.quis@viverraDonectempus.co.uk', 'Pcj28YX', '2021-03-24 12:56:21.000000'),
-(23, 'Allison', 'Valdés', '0', 'sit.amet.orci@Sedmalesuadaaugue.net', 'Ujj33AE', '2021-04-30 05:28:12.000000'),
-(24, 'Lucca', 'Gallardo', '0', 'dignissim.lacus.Aliquam@intempuseu.net', 'Mjg47XI', '2020-10-22 05:17:10.000000'),
-(25, 'Thayra', 'García', '0', 'tempor.arcu@et.org', 'Bej33TA', '2020-11-06 21:12:20.000000'),
-(26, 'Lucio', 'Campos', '0', 'consectetuer.adipiscing@scelerisque.org', 'Htc34ZR', '2021-10-20 05:47:54.000000'),
-(27, 'Jade', 'Medina', '0', 'sodales.Mauris@arcuet.edu', 'Vgv36LY', '2020-05-09 09:33:04.000000'),
-(28, 'Renato', 'Sáez', '0', 'sagittis@lectusantedictum.co.uk', 'Fps58BV', '2020-02-13 04:34:21.000000'),
-(29, 'Ashly', 'Garrido', '0', 'Quisque@eumetusIn.net', 'Duz90VA', '2020-03-22 07:45:41.000000'),
-(30, 'Andrew', 'Pérez', '0', 'posuere.vulputate.lacus@diam.com', 'Uvo05XL', '2021-06-19 00:25:57.000000'),
-(31, 'Fernando', 'Riquelme', '0', 'eu@Maurisquis.co.uk', 'Hma73QJ', '2020-05-19 06:07:14.000000'),
-(32, 'Rosita', 'Moreno', '0', 'viverra.Maecenas.iaculis@pellentesquetellussem.ca', 'Lvu19UR', '2021-01-04 16:58:43.000000'),
-(33, 'Chriss', 'San Martín', '0', 'ornare.lectus.ante@erat.com', 'Jyv84PU', '2021-04-06 05:09:05.000000'),
-(34, 'Maytte', 'Cortés', '0', 'eget@sem.edu', 'Fid49KZ', '2019-12-11 22:39:47.000000'),
-(35, 'Loreto', 'Palma', '0', 'malesuada.Integer.id@volutpatNulladignissim.edu', 'Fnu22FO', '2020-12-22 09:05:32.000000'),
-(36, 'Lesly', 'Vega', '0', 'non@facilisisfacilisismagna.org', 'Qsd49GF', '2021-08-17 15:49:56.000000'),
-(37, 'Cecilia', 'Ramírez', '0', 'Nulla.interdum.Curabitur@fringillacursus.edu', 'Gcf29ZA', '2020-08-29 01:27:04.000000'),
-(38, 'Jeans', 'Farías', '0', 'non@acfeugiatnon.net', 'Yha55EE', '2020-11-16 17:34:28.000000'),
-(39, 'Marthyna', 'Donoso', '0', 'vestibulum.lorem@NullaaliquetProin.ca', 'Ilt28DL', '2020-12-12 06:15:22.000000'),
-(40, 'Celso', 'Guerrero', '0', 'adipiscing.enim.mi@ultriciesdignissim.co.uk', 'Nfj13EG', '2020-02-20 12:32:23.000000'),
-(41, 'Giorgio', 'Morales', '0', 'laoreet@temporaugueac.ca', 'Lvj19LZ', '2021-06-28 06:11:00.000000'),
-(42, 'Ninoska', 'Pérez', '0', 'ipsum@asollicitudinorci.com', 'Pqf40SP', '2021-01-15 10:53:52.000000'),
-(43, 'Paola', 'Navarrete', '0', 'diam@nislelementumpurus.edu', 'Fgb45DS', '2020-03-01 22:25:25.000000'),
-(44, 'Adonis', 'Vidal', '0', 'nec.quam@Duisat.net', 'Thd68UU', '2020-12-09 05:31:02.000000'),
-(45, 'Andrés', 'Ortiz', '0', 'et.risus@erosProinultrices.co.uk', 'Kbo38EG', '2020-07-26 21:26:46.000000'),
-(46, 'Nahir', 'Salazar', '0', 'molestie.Sed@ac.ca', 'Xqt25HJ', '2020-04-06 09:24:06.000000'),
-(47, 'Joakin', 'Vargas', '0', 'ornare.lectus.ante@ametmetus.ca', 'Tel02WJ', '2020-03-17 18:55:54.000000'),
-(48, 'Karina', 'Miranda', '0', 'sit@bibendumfermentummetus.ca', 'Gsv36UO', '2021-01-07 06:55:06.000000'),
-(49, 'Dilan', 'Pérez', '0', 'iaculis.nec.eleifend@Donecnibh.edu', 'Utx58FO', '2021-02-08 07:40:14.000000'),
-(50, 'Rosita', 'Ramírez', '0', 'massa.Vestibulum@infaucibusorci.org', 'Qku61PO', '2020-11-10 12:49:43.000000'),
-(51, 'Jared', 'Jiménez', '0', 'enim.Etiam.imperdiet@ornareelitelit.org', 'Joq18LP', '2021-01-01 00:54:49.000000'),
-(52, 'Carolaine', 'Leiva', '0', 'eget@consequat.com', 'Xzy66LZ', '2020-01-25 05:59:49.000000'),
-(53, 'Rosita', 'Garrido', '0', 'tellus@Duis.edu', 'Nrz35IO', '2020-03-08 03:19:26.000000'),
-(54, 'Jahir', 'Flores', '0', 'lobortis.ultrices@enimnislelementum.org', 'Cjc08DE', '2020-04-17 02:50:56.000000');
+INSERT INTO `colaborador` (`idColaborador`, `nombre`, `apellidos`, `fotoPerfil`, `email`, `password`, `fechaCreacion`, `tutorial`) VALUES
+(1, 'admin', 'admin', '0', 'admin@admin.com', 'admin', '2020-10-31 17:40:06.000000', 0),
+(2, 'Eduardo', 'Ibacache González', '0', 'eduardo@gmail.com', '1235', '2020-10-31 17:40:50.430169', 0),
+(3, 'Karina', 'Hermosilla Moraga', '0', 'kari@kari.com', 'kari', '2020-11-03 02:15:33.655545', 0),
+(4, 'Felipe', 'Javier', '0', 'Ad@ad.com', 'jiji', '2020-11-04 20:42:24.519724', 0),
+(5, 'Omar', 'Valdés', '0', 'venenatis@elementum.ca', 'Nki98MN', '2021-02-11 01:37:01.000000', 0),
+(6, 'Adela', 'Sepúlveda', '0', 'tempor.diam@felispurusac.net', 'Zce68OY', '2020-01-31 12:48:08.000000', 0),
+(7, 'Pilar', 'Campos', '0', 'tempus.non.lacinia@ullamcorpermagnaSed.net', 'Mkg27FP', '2019-12-21 18:54:18.000000', 0),
+(8, 'Eugenio', 'Muñoz', '0', 'risus.odio.auctor@tortorNunccommodo.com', 'Xrk40OK', '2021-10-10 04:54:03.000000', 0),
+(9, 'Miley', 'Figueroa', '0', 'vitae.sodales.at@placerateget.co.uk', 'Rxd56IA', '2021-02-23 10:42:18.000000', 0),
+(10, 'Yeremy', 'Silva', '0', 'Nulla.eget.metus@arcuac.net', 'Jrz65DT', '2019-11-29 21:02:13.000000', 0),
+(11, 'Isaias', 'Araya', '0', 'cubilia.Curae@etpede.com', 'Etx46JQ', '2021-01-09 12:44:55.000000', 0),
+(12, 'Patrick', 'Sepúlveda', '0', 'dui@rutrumeuultrices.ca', 'Nba93HV', '2019-12-15 01:38:44.000000', 0),
+(13, 'Ademir', 'Jiménez', '0', 'cursus.diam@mattis.edu', 'Mod02XD', '2021-04-21 07:24:06.000000', 0),
+(14, 'Joao', 'Martínez', '0', 'aliquet.libero@eutellusPhasellus.org', 'Rub72FA', '2020-07-30 15:43:58.000000', 0),
+(15, 'Cristián', 'Ortega', '0', 'ut.nisi@senectus.co.uk', 'Qec12PZ', '2021-02-04 18:15:29.000000', 0),
+(16, 'Yanira', 'Vega', '0', 'nec@magnis.co.uk', 'Nty25JM', '2020-11-10 20:13:12.000000', 0),
+(17, 'Lorenzo', 'Vásquez', '0', 'Nullam@Nuncquisarcu.net', 'Bit58TU', '2020-09-07 23:15:00.000000', 0),
+(18, 'Maria', 'Muñoz', '0', 'augue.ut.lacus@sapien.edu', 'Wnz15IA', '2021-05-26 03:49:12.000000', 0),
+(19, 'Thiago', 'Alvarado', '0', 'nulla@ultriciesligulaNullam.ca', 'Tsd73VK', '2020-08-02 06:44:49.000000', 0),
+(20, 'Jean', 'Silva', '0', 'Donec.felis@sollicitudincommodo.co.uk', 'Lqy79KE', '2020-01-02 18:45:26.000000', 0),
+(21, 'Michelle', 'Carvajal', '0', 'Suspendisse@Morbi.org', 'Wqs37IT', '2020-11-11 21:17:06.000000', 0),
+(22, 'Amaru', 'Salazar', '0', 'lobortis.quis@viverraDonectempus.co.uk', 'Pcj28YX', '2021-03-24 12:56:21.000000', 0),
+(23, 'Allison', 'Valdés', '0', 'sit.amet.orci@Sedmalesuadaaugue.net', 'Ujj33AE', '2021-04-30 05:28:12.000000', 0),
+(24, 'Lucca', 'Gallardo', '0', 'dignissim.lacus.Aliquam@intempuseu.net', 'Mjg47XI', '2020-10-22 05:17:10.000000', 0),
+(25, 'Thayra', 'García', '0', 'tempor.arcu@et.org', 'Bej33TA', '2020-11-06 21:12:20.000000', 0),
+(26, 'Lucio', 'Campos', '0', 'consectetuer.adipiscing@scelerisque.org', 'Htc34ZR', '2021-10-20 05:47:54.000000', 0),
+(27, 'Jade', 'Medina', '0', 'sodales.Mauris@arcuet.edu', 'Vgv36LY', '2020-05-09 09:33:04.000000', 0),
+(28, 'Renato', 'Sáez', '0', 'sagittis@lectusantedictum.co.uk', 'Fps58BV', '2020-02-13 04:34:21.000000', 0),
+(29, 'Ashly', 'Garrido', '0', 'Quisque@eumetusIn.net', 'Duz90VA', '2020-03-22 07:45:41.000000', 0),
+(30, 'Andrew', 'Pérez', '0', 'posuere.vulputate.lacus@diam.com', 'Uvo05XL', '2021-06-19 00:25:57.000000', 0),
+(31, 'Fernando', 'Riquelme', '0', 'eu@Maurisquis.co.uk', 'Hma73QJ', '2020-05-19 06:07:14.000000', 0),
+(32, 'Rosita', 'Moreno', '0', 'viverra.Maecenas.iaculis@pellentesquetellussem.ca', 'Lvu19UR', '2021-01-04 16:58:43.000000', 0),
+(33, 'Chriss', 'San Martín', '0', 'ornare.lectus.ante@erat.com', 'Jyv84PU', '2021-04-06 05:09:05.000000', 0),
+(34, 'Maytte', 'Cortés', '0', 'eget@sem.edu', 'Fid49KZ', '2019-12-11 22:39:47.000000', 0),
+(35, 'Loreto', 'Palma', '0', 'malesuada.Integer.id@volutpatNulladignissim.edu', 'Fnu22FO', '2020-12-22 09:05:32.000000', 0),
+(36, 'Lesly', 'Vega', '0', 'non@facilisisfacilisismagna.org', 'Qsd49GF', '2021-08-17 15:49:56.000000', 0),
+(37, 'Cecilia', 'Ramírez', '0', 'Nulla.interdum.Curabitur@fringillacursus.edu', 'Gcf29ZA', '2020-08-29 01:27:04.000000', 0),
+(38, 'Jeans', 'Farías', '0', 'non@acfeugiatnon.net', 'Yha55EE', '2020-11-16 17:34:28.000000', 0),
+(39, 'Marthyna', 'Donoso', '0', 'vestibulum.lorem@NullaaliquetProin.ca', 'Ilt28DL', '2020-12-12 06:15:22.000000', 0),
+(40, 'Celso', 'Guerrero', '0', 'adipiscing.enim.mi@ultriciesdignissim.co.uk', 'Nfj13EG', '2020-02-20 12:32:23.000000', 0),
+(41, 'Giorgio', 'Morales', '0', 'laoreet@temporaugueac.ca', 'Lvj19LZ', '2021-06-28 06:11:00.000000', 0),
+(42, 'Ninoska', 'Pérez', '0', 'ipsum@asollicitudinorci.com', 'Pqf40SP', '2021-01-15 10:53:52.000000', 0),
+(43, 'Paola', 'Navarrete', '0', 'diam@nislelementumpurus.edu', 'Fgb45DS', '2020-03-01 22:25:25.000000', 0),
+(44, 'Adonis', 'Vidal', '0', 'nec.quam@Duisat.net', 'Thd68UU', '2020-12-09 05:31:02.000000', 0),
+(45, 'Andrés', 'Ortiz', '0', 'et.risus@erosProinultrices.co.uk', 'Kbo38EG', '2020-07-26 21:26:46.000000', 0),
+(46, 'Nahir', 'Salazar', '0', 'molestie.Sed@ac.ca', 'Xqt25HJ', '2020-04-06 09:24:06.000000', 0),
+(47, 'Joakin', 'Vargas', '0', 'ornare.lectus.ante@ametmetus.ca', 'Tel02WJ', '2020-03-17 18:55:54.000000', 0),
+(48, 'Karina', 'Miranda', '0', 'sit@bibendumfermentummetus.ca', 'Gsv36UO', '2021-01-07 06:55:06.000000', 0),
+(49, 'Dilan', 'Pérez', '0', 'iaculis.nec.eleifend@Donecnibh.edu', 'Utx58FO', '2021-02-08 07:40:14.000000', 0),
+(50, 'Rosita', 'Ramírez', '0', 'massa.Vestibulum@infaucibusorci.org', 'Qku61PO', '2020-11-10 12:49:43.000000', 0),
+(51, 'Jared', 'Jiménez', '0', 'enim.Etiam.imperdiet@ornareelitelit.org', 'Joq18LP', '2021-01-01 00:54:49.000000', 0),
+(52, 'Carolaine', 'Leiva', '0', 'eget@consequat.com', 'Xzy66LZ', '2020-01-25 05:59:49.000000', 0),
+(53, 'Rosita', 'Garrido', '0', 'tellus@Duis.edu', 'Nrz35IO', '2020-03-08 03:19:26.000000', 0),
+(54, 'Jahir', 'Flores', '0', 'lobortis.ultrices@enimnislelementum.org', 'Cjc08DE', '2020-04-17 02:50:56.000000', 0),
+(55, 'Este', 'bon bon', '0', 'este@gmail.com', '123', '2020-11-05 23:18:29.336377', 0);
 
 -- --------------------------------------------------------
 
@@ -198,7 +201,7 @@ CREATE TABLE `equipo` (
   `idEquipo` int(255) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `objetivo` varchar(255) DEFAULT NULL,
-  `fechaCreacion` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+  `fechaCreacion` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -240,7 +243,7 @@ CREATE TABLE `evento` (
   `descripcion` varchar(255) NOT NULL,
   `fecha` date NOT NULL,
   `hora` int(255) NOT NULL,
-  `fechaCreacion` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `fechaCreacion` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `enlaceVideoconferencia` varchar(255) DEFAULT NULL,
   `idCategoria` int(255) DEFAULT NULL,
   `idEquipo` int(255) NOT NULL,
@@ -296,7 +299,8 @@ CREATE TABLE `listatareas` (
 --
 
 INSERT INTO `listatareas` (`idListaTareas`, `idTarea`, `idColaborador`) VALUES
-(1, 1, 3);
+(1, 1, 3),
+(2, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -308,7 +312,7 @@ CREATE TABLE `tarea` (
   `idTarea` int(255) NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `fecha` date NOT NULL,
-  `fechaCreacion` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `fechaCreacion` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `descripcion` varchar(255) NOT NULL,
   `idCategoria` int(255) DEFAULT NULL,
   `idEquipo` int(255) NOT NULL
@@ -319,7 +323,8 @@ CREATE TABLE `tarea` (
 --
 
 INSERT INTO `tarea` (`idTarea`, `nombre`, `fecha`, `fechaCreacion`, `descripcion`, `idCategoria`, `idEquipo`) VALUES
-(1, 'Comer mucho', '2020-11-26', '2020-11-05 02:33:03.000000', 'La idea es comer mucho', 1, 1);
+(1, 'Comer mucho', '2020-11-26', '2020-11-05 02:33:03.000000', 'La idea es comer mucho', 1, 1),
+(2, 'Comer poco', '2020-11-06', '0000-00-00 00:00:00.000000', 'Comer poco xq si', 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -411,7 +416,7 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de la tabla `colaborador`
 --
 ALTER TABLE `colaborador`
-  MODIFY `idColaborador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `idColaborador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT de la tabla `equipo`
@@ -447,7 +452,7 @@ ALTER TABLE `listatareas`
 -- AUTO_INCREMENT de la tabla `tarea`
 --
 ALTER TABLE `tarea`
-  MODIFY `idTarea` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `idTarea` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
