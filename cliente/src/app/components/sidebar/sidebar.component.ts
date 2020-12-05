32f22c09f7d41c3a0cde84dda41f870fdc4ab0b2
Bastian Lara <nicolaramedina@gmail.com>
2020-12-05 01:46:32 -0300

    campanitas funcionando

--- cliente/src/app/components/sidebar/sidebar.component.ts
+++ cliente/src/app/components/sidebar/sidebar.component.ts
@@ -27,7 +27,7 @@ export class SidebarComponent implements OnInit {
       });
   }
   queMostrar(){
-    if(!this.notifiacion){
+    if(this.notifiacion.length==0){
       this.mostrarNot=false;
       console.log("mostrar campana 1");
     }else{
